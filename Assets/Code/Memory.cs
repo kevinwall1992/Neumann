3fba781fdcd75c9d83abb616e42099e41ba075bb
Kevin Wall <kwall@sci.utah.edu>
2020-03-25 14:54:34 -0600

    Adding Memory class
    Memory is for storing values not inherent to the function of a class (like health is for Mortal), making them retrievable as Variables. This is especially useful for readonly values (like storing a position when the user clicks the map) and writable values of arbitrary type (like when the user needs to store an int/string/etc). The emphasis here is values that are created and/or modified at runtime, based on user input.

---
+++ Assets/Code/Memory.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Memory : HasVariables
+{
+    Dictionary<string, object> memories = new Dictionary<string, object>();
+    HashSet<string> writable_memories = new HashSet<string>();
+
+    Dictionary<object, string> memory_names = new Dictionary<object, string>();
+
+    Dictionary<string, Variable> variables = new Dictionary<string, Variable>();
+
+    public List<Variable> Variables
+    {
+        get
+        {
+            List<Variable> new_variables = new List<Variable>();
+
+            new_variables.AddRange(memories.Keys
+                .Where(name => writable_memories.Contains(name) && !variables.ContainsKey(name))
+                .Select(name => new WritableVariable(name, memories[name])));
+
+            new_variables.AddRange(memories.Keys
+                .Where(name => !writable_memories.Contains(name) && !variables.ContainsKey(name))
+                .Select(name => new ReadOnlyVariable(name, memories[name])));
+
+            foreach (Variable variable in new_variables)
+                variables[variable.Name] = variable;
+
+            foreach (string name in variables.Keys.ToList())
+                if (!memories.ContainsKey(name))
+                    variables.Remove(name);
+
+            return variables.Values.ToList();
+        }
+    }
+
+    public void Memorize(string name, object memory, bool is_writable = false)
+    {
+        memories[name] = memory;
+        memory_names[memory] = name;
+
+        if (is_writable)
+            writable_memories.Add(name);
+        else if (writable_memories.Contains(name))
+            writable_memories.Remove(name);
+    }
+
+    public object Remember(string name)
+    {
+        if (!memories.ContainsKey(name))
+            return null;
+
+        return memories[name];
+    }
+
+    //Remembers the last Memorize()d name for memory
+    //e.g. Storing the number "7" twice means the second
+    //name you gave it will be returned here.
+    public string RememberName(object memory)
+    {
+        if (!memory_names.ContainsKey(memory))
+            return null;
+
+        return memory_names[memory];
+    }
+
+    public int Count<T>()
+    {
+        return memories.Values.Where(value => value is T).Count();
+    }
+}
