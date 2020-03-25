6427a880ec49e213fce991668d8135147438b6d7
Kevin Wall <kwall@sci.utah.edu>
2020-03-25 16:04:43 -0600

    index based Program.Next
    Made Program.Next no longer just an auto property, but backed by an index. Includes better validation of input and output.

--- Assets/Code/Program.cs
+++ Assets/Code/Program.cs
@@ -4,5 +4,32 @@ using System.Collections.Generic;
 
 public class Program : List<Operation>
 {
-    Operation Next { get; set; }
+    int next_index = -1;
+
+    public Operation Next
+    {
+        get
+        {
+            if (next_index < 0 || next_index >= Count)
+            {
+                next_index = -1;
+                return null;
+            }
+
+            return this[next_index];
+        }
+
+        set
+        {
+            if (value == null)
+                next_index = -1;
+            else if (!Contains(value))
+            {
+                Debug.Assert(false);
+                return;
+            }
+
+            next_index = IndexOf(value);
+        }
+    }
 }
