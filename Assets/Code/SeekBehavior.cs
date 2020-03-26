7868c94ba4ea9d50ee1cdf4c227fee2cb50767c7
Kevin Wall <kwall@sci.utah.edu>
2020-03-26 15:15:52 -0600

    SeekBehavior improvements
    SeekBehavior now actually gets close enough to targets to register completion of MoveTasks.

--- Assets/Code/SeekBehavior.cs
+++ Assets/Code/SeekBehavior.cs
@@ -26,41 +26,27 @@ public class SeekBehavior : Behavior
         Vector3 surface_direction = displacement.InPlane(Vector3.up).InPlane(normal).normalized;
         float parallel_velocity = Physical.Velocity.Dot(Physical.Direction);
 
-        if (displacement.magnitude < 2.5f)
-        {
-            Motile.Brake(1.0f);
+        Motile.Turn(surface_direction);
 
-            if (parallel_velocity < 0)
-                Motile.Drive();
+        if (Physical.Direction.Dot(surface_direction) > 0.9f)
+        {
+            if ((displacement.magnitude < 10 && parallel_velocity > 6) || (displacement.magnitude < 3 && parallel_velocity > 2))
+                Motile.Brake(0.8f);
             else
-                Motile.Neutral();
-
-            Motile.Turn(surface_direction);
-        }
-        else
-        {
-            Motile.Turn(surface_direction);
-
-            if (Physical.Direction.Dot(surface_direction) > 0.9f)
             {
-                if (displacement.magnitude < 8.0f)
-                {
-                    if(parallel_velocity > 6)
-                        Motile.Brake(1.0f);
-                    else
-                        Motile.Brake(0.2f);
-                }
-                else
-                    Motile.ReleaseBrake();
+                Motile.ReleaseBrake();
                 Motile.Drive();
             }
-            else
-                Motile.Brake();
         }
+        else
+            Motile.Brake();
     }
 
     protected override void OnDestroy()
     {
+        Motile.Brake();
+        Motile.Drive(0);
+
         base.OnDestroy();
     }
 }
