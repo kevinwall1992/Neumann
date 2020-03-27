f9a7a345e18c16e60218d5ffd17545ccdd313016
Kevin Wall <kwall@sci.utah.edu>
2020-03-27 11:33:40 -0600

    Getting rid of OperationTile private properties
    Private properties with no likelihood of being made public don't fit the coding style.

--- Assets/Code/OperationTile.cs
+++ Assets/Code/OperationTile.cs
@@ -27,18 +27,14 @@ public class OperationTile : Tile
         }
     }
 
-    //Properties seem unnecessary if private****
     [SerializeField]
     Text description_text = null;
-    Text DescriptionText { get { return description_text; } }
 
     [SerializeField]
     Image underlay = null;
-    Image Underlay { get { return underlay; } }
 
     [SerializeField]
     Image discard_overlay = null;
-    Image DiscardOverlay { get { return discard_overlay; } }
 
     [SerializeField]
     Operation operation;
@@ -50,7 +46,7 @@ public class OperationTile : Tile
         {
             operation = value;
 
-            Underlay.color = Color.clear;
+            underlay.color = Color.clear;
 
             input_node.gameObject.SetActive(false);
             output_node.gameObject.SetActive(false);
@@ -60,7 +56,7 @@ public class OperationTile : Tile
             {
                 Image.sprite = Operation.Style.Sprite;
                 Image.color = Operation.Style.Color;
-                DescriptionText.text = Operation.Style.Description;
+                description_text.text = Operation.Style.Description;
 
                 if (Operation.TakesInput)
                     input_node.VariableTile = VariableTile.Find(Operation.Input.PrimaryVariableName);
@@ -76,14 +72,14 @@ public class OperationTile : Tile
                     BuildTask build_task = Operation as BuildTask;
                     Unit unit_blueprint = build_task.Blueprint.GetComponent<Unit>();
 
-                    Underlay.sprite = unit_blueprint.Icon;
+                    underlay.sprite = unit_blueprint.Icon;
                 }
             }
             else
             {
                 Image.sprite = null_task_sprite;
                 Image.color = Color.white;
-                DescriptionText.text = "";
+                description_text.text = "";
             }
         }
     }
@@ -122,7 +118,7 @@ public class OperationTile : Tile
         //Have to do this because Tile prefab is super-prefab
         //to OperationTile prefab, and underlay can't come first
         //in the editor because of prefab rules. 
-        Underlay.transform.SetAsFirstSibling();
+        underlay.transform.SetAsFirstSibling();
 
         input_node.gameObject.SetActive(false);
         output_node.gameObject.SetActive(false);
@@ -135,9 +131,9 @@ public class OperationTile : Tile
             return;
 
         if (Unit != null && Operation is Task && (Operation as Task) is BuildTask)
-            Underlay.color = Unit.Team.Color;
+            underlay.color = Unit.Team.Color;
 
-        DescriptionText.gameObject.SetActive(this.IsPointedAt());
+        description_text.gameObject.SetActive(this.IsPointedAt());
   
         SelectionOverlay.gameObject.SetActive(IsSelected);
 
@@ -257,12 +253,12 @@ public class OperationTile : Tile
         if ((Scene.Main.UnitInterface.ProgramInterface.transform as RectTransform).ContainsMouse())
         {
             Scene.Main.UnitInterface.ProgramInterface.PreviewAt(Input.mousePosition);
-            DiscardOverlay.gameObject.SetActive(false);
+            discard_overlay.gameObject.SetActive(false);
         }
         else
         {
             Scene.Main.UnitInterface.ProgramInterface.StopPreviewing();
-            DiscardOverlay.gameObject.SetActive(true);
+            discard_overlay.gameObject.SetActive(true);
         }
     }
 
@@ -273,7 +269,7 @@ public class OperationTile : Tile
 
         base.OnEndDrag(eventData);
 
-        DiscardOverlay.gameObject.SetActive(false);
+        discard_overlay.gameObject.SetActive(false);
 
         if ((Scene.Main.UnitInterface.ProgramInterface.transform as RectTransform).ContainsMouse())
             Scene.Main.UnitInterface.ProgramInterface.AddAt(this, Input.mousePosition);
