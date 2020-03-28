5dd3fb2136ecc85964a47df947cef2c6c2e5cf44
Kevin Wall <kwall@sci.utah.edu>
2020-03-28 13:31:27 -0600

    CircleLineController
    Added new class that directs a LineRenderer to draw a circle.

---
+++ Assets/Code/CircleLineController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteAlways]
+public class CircleLineController : MonoBehaviour
+{
+    [SerializeField]
+    LineRenderer line = null;
+
+    public int SampleCount = 20;
+
+    void Start()
+    {
+        line.loop = true;
+    }
+
+    void Update()
+    {
+        line.positionCount = SampleCount;
+        for (int i = 0; i < SampleCount; i++)
+        {
+            float radians = 2 * Mathf.PI * i / SampleCount;
+
+            line.SetPosition(i, new Vector3(Mathf.Sin(radians), 0, Mathf.Cos(radians)));
+        }
+    }
+}
