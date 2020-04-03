9491547edc844c805c12fe7789dba369d138dd3f
Kevin Wall <kwall@sci.utah.edu>
2020-04-03 11:24:48 -0600

    RequestBehavior
    Added new class to simplify Behaviors that make use of Stock.Requests.
    
    Also added subclass which specifically handles energy requests.

---
+++ Assets/Code/RequestBehavior.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class RequestBehavior: Behavior
+{
+    Stock.Request request;
+
+    public abstract Pile BaseUsagePerSecond { get; }
+    public virtual float UsageFraction { get { return 1; } }
+
+    public float YieldAdjustedUsageFraction
+    {
+        get { return request.Yield * request.UsagePerSecond.Volume / BaseUsagePerSecond.Volume; }
+    }
+
+    public Unit Unit { get { return GetComponent<Unit>(); } }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        request = Unit.Team.Stock.MakeRequest();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        request.UsagePerSecond = BaseUsagePerSecond * UsageFraction;
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        request.Revoke();
+    }
+}
+
+public abstract class EnergyRequestBehavior : RequestBehavior
+{
+    public abstract float EnergyPerSecond { get; }
+
+    public override Pile BaseUsagePerSecond
+    {
+        get { return new Pile().PutIn(Resource.Energy, EnergyPerSecond); }
+    }
+}
