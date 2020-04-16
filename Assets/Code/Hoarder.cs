5504e1e431234ab46b52fc39ee0d774d6c1a0540
Kevin Wall <kwall@sci.utah.edu>
2020-04-16 15:22:00 -0600

    Hoarder
    This class allows Units to have their own storage space, separate from the Team.Stock.

---
+++ Assets/Code/Hoarder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Unit))]
+public class Hoarder : MonoBehaviour
+{
+    public Pile Capacity { get; private set; } = new Pile();
+    public Pile Contents { get; private set; } = new Pile();
+
+    public Unit Unit { get { return GetComponent<Unit>(); } }
+
+    private void Update()
+    {
+        foreach (Resource resource in new List<Resource>(Contents.Resources))
+        {
+            float volume = Contents.GetVolumeOf(resource);
+            float capacity = Capacity.GetVolumeOf(resource);
+
+            if (volume > capacity)
+                Unit.Team.Stock.Pile.PutIn(Contents.TakeOut(resource, volume - capacity));
+        }
+    }
+}
