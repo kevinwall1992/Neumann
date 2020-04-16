4b66338819bf11e28c6c916ca4bf10a7eefdf60b
Kevin Wall <kwall@sci.utah.edu>
2020-04-16 15:26:52 -0600

    MapSymbol
    This class enables scaling of an object to a specified fraction of the screen's vertical size.
    
    Intended for representing small/distant objects as much larger icons (to increase visibility).

---
+++ Assets/Code/MapSymbol.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Billboard))]
+public class MapSymbol : MonoBehaviour
+{
+    public float RelativeScreenSpaceSize;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float radians = MathUtility.DegreesToRadians(Scene.Main.Camera.fieldOfView / 2);
+        float world_height_at_distance =
+            Scene.Main.Camera.transform.position.Distance(transform.position) *
+            2 * Mathf.Sin(radians) /
+            Mathf.Cos(radians);
+
+        transform.localScale = new Vector3(1, 1, 1) *
+                               RelativeScreenSpaceSize *
+                               world_height_at_distance /
+                               transform.parent.lossyScale.y;
+    }
+}
