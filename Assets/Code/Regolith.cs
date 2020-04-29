55aa42f62d59eec7b85a6776e3af7f8460a80e8b
Kevin Wall <kwall@sci.utah.edu>
2020-04-29 12:40:23 -0600

    Regolith changes
    Removed GetVolumeWithinRange() and GetConcentrationWithinRange() overrides.
    
    They existed to make Regolith act like the Surface while occluded by the Surface, but these two methods really only prevent querying info about specifically the Regolith while not being necessary in taking an occluded sample.

--- Assets/Code/Regolith.cs
+++ Assets/Code/Regolith.cs
@@ -9,24 +9,6 @@ public class Regolith : Stratum
             .GetVolumeWithinRange(position, range) > 0;
     }
 
-    public override float GetVolumeWithinRange(Vector3 position, float range, Resource resource = null)
-    {
-        if (IsOccluded(position, range))
-            return Scene.Main.World.Asteroid.Surface
-                .GetVolumeWithinRange(position, range, resource);
-
-        return base.GetVolumeWithinRange(position, range, resource);
-    }
-
-    public override float GetConcentrationByVolume(Vector3 position, float range, Resource resource)
-    {
-        if (IsOccluded(position, range))
-            return Scene.Main.World.Asteroid.Surface
-                .GetConcentrationByVolume(position, range, resource);
-
-        return base.GetConcentrationByVolume(position, range, resource);
-    }
-
     public override Pile TakeSample(Vector3 position, float range, float volume)
     {
         if (IsOccluded(position, range))
